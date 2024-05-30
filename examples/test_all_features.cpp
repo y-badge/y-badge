94f127e4eb1f6b45ec712ec6c64f3dc1e884ffa9
Philip Lundrigan <lundrigan@byu.edu>
2024-05-30 10:26:32 -0600

    Update all features example

--- examples/test_all_features.cpp
+++ examples/test_all_features.cpp
@@ -1,71 +1,35 @@
 
-#include "ybadge.h"
-
-int get_brightness();
-void all_leds_set_color(int r, int g, int b);
-void turn_off_all_lights();
-bool check_switches();
+#include "yboard.h"
 
 void setup() {
     Serial.begin(9600);
-    leds_init();
-    timer_init();
+    Yboard.setup();
 }
 
-/*
- * This function tests all of the features of the badge.
- * It will display different colors depending on which button is pressed.
- * It will also play different notes depending on which button is pressed IF at
- * least one of the two switches is on It will also change the brightness of the
- * LEDs depending on the position of the knob.
- */
 void loop() {
-    if (buttons_get(1)) {
-        while (buttons_get(1)) {
-            all_leds_set_color(255, 0, 0);
-            leds_set_brightness(get_brightness());
-            if (check_switches()) {
-                speaker_play_note(NOTE_C4, 10);
-                delay(10);
-            }
-        }
-    } else if (buttons_get(2)) {
-        while (buttons_get(2)) {
-            all_leds_set_color(255, 255, 0);
-            leds_set_brightness(get_brightness());
-            if (check_switches()) {
-                speaker_play_note(NOTE_D4, 10);
-                delay(10);
-            }
-        }
-    } else if (buttons_get(3)) {
-        while (buttons_get(3)) {
-            all_leds_set_color(0, 255, 0);
-            leds_set_brightness(get_brightness());
-            if (check_switches()) {
-                speaker_play_note(NOTE_E4, 10);
-                delay(10);
-            }
-        }
+    if (Yboard.get_switch(1)) {
+        Yboard.set_led_color(0, 255, 0, 0);
     } else {
-        all_leds_set_color(255, 255, 255);
-        leds_set_brightness(get_brightness());
+        Yboard.set_led_color(0, 0, 0, 0);
     }
-}
 
-// This function converts the knob's output (1-100) to a brightness value
-// (0-255)
-int get_brightness() {
-    int brightness = 255 * knob_get() / 100;
-    return floor(brightness);
-}
-
-// This function lights up all of the LEDs with the given color
-void all_leds_set_color(int r, int g, int b) {
-    for (int i = 1; i <= 20; i++) {
-        leds_set_color(i, r, g, b);
+    if (Yboard.get_switch(2)) {
+        Yboard.set_led_color(1, 255, 0, 0);
+    } else {
+        Yboard.set_led_color(1, 0, 0, 0);
     }
-}
 
-// This function checks if either of the switches are on
-bool check_switches() { return switches_get(1) || switches_get(2); }
+    if (Yboard.get_button(1)) {
+        Yboard.set_led_color(2, 255, 0, 0);
+    } else {
+        Yboard.set_led_color(2, 0, 0, 0);
+    }
+
+    if (Yboard.get_button(2)) {
+        Yboard.set_led_color(3, 255, 0, 0);
+    } else {
+        Yboard.set_led_color(3, 0, 0, 0);
+    }
+
+    Yboard.set_led_color(4, map(Yboard.get_knob(), 0, 100, 0, 255), 0, 0);
+}
